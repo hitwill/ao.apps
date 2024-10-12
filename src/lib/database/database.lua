78c3fcb4410a77ed517addd878e7760b45ec9e74
skyfoxx <skyfoxx@sacred.finance>
2024-10-12 17:31:45 +1100

    feat: CRUD functions

--- src/lib/database/database.lua
+++ src/lib/database/database.lua
@@ -78,3 +78,85 @@ Handlers.add(
     })
   end
 )
+
+
+Handlers.add(
+  "updateone",
+  { Action = "UpdateOne" },
+  --[[
+   "Data": {
+      "table": "resources",
+      "set": "name = ?, description = ?, votes = ?",
+      "values": ["Updated Resource Name", "This is an updated description", 10, 1]
+    }
+  ]]
+  function(msg)
+    local data = json.decode(msg.Data)
+    local query = string.format("UPDATE %s SET %s WHERE id = ?", data.table, data.set)
+    local stmt = DB:prepare(query)
+    stmt:bind_values(table.unpack(data.values))
+    local response = stmt:step()
+    stmt:finalize()
+    local result = json.encode({ response = response })
+
+    ao.send({
+      Target = msg.From,
+      Data = result
+    })
+  end
+)
+
+Handlers.add(
+  "deleteone",
+  { Action = "DeleteOne" },
+  --[[
+    "Data": {
+      "table": "resources",
+      "id": 1
+    }
+  ]]
+  function(msg)
+    local data = json.decode(msg.Data)
+    local query = string.format("DELETE FROM %s WHERE id = ?", data.table)
+    local stmt = DB:prepare(query)
+    stmt:bind_values(data.id)
+    local response = stmt:step()
+    stmt:finalize()
+    local result = json.encode({ response = response })
+
+    ao.send({
+      Target = msg.From,
+      Data = result
+    })
+  end
+)
+
+Handlers.add(
+  "fetchone",
+  { Action = "FetchOne" },
+  --[[
+    "Data": {
+      "table": "resources",
+      "id": 1
+    }
+  ]]
+  function(msg)
+    local data = json.decode(msg.Data)
+    local query = string.format("SELECT * FROM %s WHERE id = ? LIMIT 1", data.table)
+    local stmt = DB:prepare(query)
+    stmt:bind_values(data.id)
+    local row = stmt:step()
+    local result = {}
+    if row then
+      for i = 0, stmt:columns() - 1 do
+        result[stmt:column_name(i)] = stmt:column_value(i)
+      end
+    end
+    stmt:finalize()
+
+    ao.send({
+      Target = msg.From,
+      Data = json.encode(result)
+    })
+  end
+)
