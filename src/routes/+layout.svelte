6991cc68858634d10040d111a08f2f06c18c469b
skyfoxx <skyfoxx@sacred.finance>
2024-10-14 14:41:42 +1100

    chore: remove unused

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,18 +8,6 @@
 </main>
 
 <style>
-    nav {
-        padding: 1rem;
-        background-color: #f0f0f0;
-    }
-    a {
-        margin-right: 1rem;
-        color: #333;
-        text-decoration: none;
-    }
-    .active {
-        font-weight: bold;
-    }
     main {
         padding: 1rem;
     }
