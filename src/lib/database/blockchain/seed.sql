fc29e16a2029a35583ce7e5630163a8be43a2ac5
skyfoxx <skyfoxx@sacred.finance>
2024-10-14 17:29:10 +1100

    feat: add seed

---
+++ src/lib/database/blockchain/seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO resources
+  (name, link, description, type, category, tags, ecosystem)
+VALUES
+  ('ArDrive', 'https://ardrive.io', 'Cloud storage service built on Arweave, offering permanent data storage.', 'application', 'storage', 'storage, cloud-storage, arweave', 'arweave'),
+  ('WeaveTransfer', 'https://weavetransfer.othent.io', 'A simple tool for transferring files using Arweave.', 'application', 'tool', 'file-sharing, transfer', 'arweave'),
+  ('Akord', 'https://akord.com', 'A user-friendly platform for storing and publishing files on Arweave.', 'application', 'storage', 'storage, publishing', 'arweave'),
+  ('Arweave Kit', 'https://www.arweavekit.com', 'A comprehensive library of tools for developers building on Arweave.', 'library', 'library', 'sdk, developer-tools', 'arweave'),
+  ('ArweaveLists', 'https://list.weavescan.com', 'A comprehensive library of Arweave applications, allowing easy discovery.', 'website', 'community', 'discovery, applications', 'arweave'),
+  ('PermaFacts', 'https://permafacts.arweave.dev', 'A neutral publishing platform that allows users to publish facts on the blockchain.', 'application', 'content', 'social, publishing', 'arweave'),
+  ('ArConnect', 'https://www.arconnect.io', 'A wallet extension for managing Arweave accounts and transactions.', 'wallet', 'tool', 'wallet, transactions', 'arweave'),
+  ('Othent', 'https://othent.io', 'A fast way to create an Arweave account using social logins.', 'application', 'identity', 'identity, authentication', 'arweave'),
+  ('Arweave.org', 'https://www.arweave.org', 'The official website of the Arweave project, offering insights and resources.', 'website', 'community', 'information, community', 'arweave'),
+  ('AO Documentation', 'https://ao.arweave.dev/docs', 'Official documentation for AO, a decentralized computing platform.', 'documentation', 'education', 'documentation, tutorials', 'ao'),
+  ('Arweave Explorer', 'https://explorer.arweave.dev', 'A blockchain explorer for browsing transactions on the Arweave network.', 'explorer', 'tool', 'explorer, blockchain', 'arweave'),
+  ('Bazar', 'https://bazar.arweave.dev', 'A decentralized marketplace built on Arweave for buying and selling digital goods.', 'application', 'marketplace', 'marketplace, digital-goods', 'arweave'),
+  ('WeaveDB', 'https://weavedb.arweave.dev', 'A scalable, decentralized database built on Arweave.', 'application', 'storage', 'database, storage', 'arweave'),
+  ('Darkblock', 'https://darkblock.io', 'A platform for creating and managing NFTs on Arweave.', 'application', 'nft', 'nft, digital-art', 'arweave'),
+  ('Arweave Archive', 'https://archive.arweave.dev', 'A free and permanent URL shortening service for Web3 transactions.', 'application', 'storage', 'archive, historical-data', 'arweave')
